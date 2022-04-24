d3082697b3048a7a6ff160285acdc7bc36c07e95
Miller <eotjd8721@naver.com>
2022-04-24 22:30:51 +0900

    [ADD] gitignore 추가

---
+++ GraduationView.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  TidalWaveTea
+//
+//  Created by DaeSeong on 2022/04/24.
+//
+
+import SwiftUI
+
+
+struct GraduationView :  View {
+    
+    
+    var body: some View {
+    
+        VStack{
+               
+            Spacer()
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+            Divider().frame(width: UIScreen.main.bounds.width, height: 100)
+
+            }
+    }
+}
